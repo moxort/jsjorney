a2620e7f59707ba0b7aa6f5198fcf2b70f8518d2
v.petlovyi <vladyslav.petlovyi@playtech.com>
2024-10-10 12:18:05 +0300

    Day 1

---
+++ helloworld.js
@@ -0,0 +1,1 @@
+console.log('Hello, World!, executed in helloworld file');
---
+++ introduction.js
@@ -0,0 +1,13 @@
+// comments can make code more readable
+// Welcome to js
+/*
+    This is a multi-line comment,
+    it can make code reasier to read and maintain
+ */
+
+const name = "Bob"
+const lie = true;
+const senceOfLife = undefined;
+const numberOfKids = null;
+
+console.log(typeof name,typeof lie,typeof senceOfLife,typeof numberOfKids);
