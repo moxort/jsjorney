2de17e7f269f2f27b589aafadc25fed6170b9d94
v.petlovyi <vladyslav.petlovyi@playtech.com>
2024-10-14 14:39:11 +0300

    day 3 completed

---
+++ day3.js
@@ -0,0 +1,33 @@
+// let number = prompt('Enter number', '')
+// console.log(number)
+//
+// const agree = confirm('Are you sure you like to delete? ')
+// console.log(agree) // result will be true or false based on what you click on the dialog box
+
+/*const now = new Date()
+console.log(now)
+console.log(now.getFullYear())
+console.log(now.getMonth()) // month go from 0 to 11
+console.log(now.getDate()) // days go 1-31
+console.log(now.getDay())// weekday go 0-6
+console.log(now.getHours())// just xx number
+console.log(now.getMinutes())
+console.log(now.getSeconds())
+console.log(now.getTime())//same, unix time
+console.log(Date.now())// same, unix time*/
+// console.log(now)
+
+
+//Exercises
+const firstNAme = 'Goba', country = 'Sudan', city= 'Belgrad', age = 23, isMarried = false, year = '2024'
+// console.log(typeof firstNAme)
+// console.log(typeof country)
+// console.log(typeof city)
+// console.log(typeof age)
+// console.log(typeof isMarried)
+// console.log(typeof year)
+
+// console.log(typeof '10' === typeof 10)
+
+let base = prompt('Enter base',), height = prompt('Enter height',)
+alert(`The area of the triangle is ${0.5*height*base}`)
