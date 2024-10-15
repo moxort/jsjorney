f84b841730101d96fc0949eddd743b8c41452e1a
v.petlovyi <vladyslav.petlovyi@playtech.com>
2024-10-15 14:42:10 +0300

    day 4 completed

---
+++ day4.js
@@ -0,0 +1,17 @@
+// let age = prompt('Enter your age:')
+// if (age > 17){
+//         alert('YOu are good to drive!')
+// }else alert('YOu are NOT good to drive!')
+
+// let age = prompt('Enter your age:')
+// if (age > 20){
+//         alert(`You are ${age - 20} year(s) older then me!`)
+// }else alert(`You are ${20 -age} year(s) younger then me!`)
+
+let a = 4
+let b = 3
+
+if (a > b) console.log(a + ' bigger than ' +b)
+else console.log(b + ' bigger than ' + a)
+
+a > b ?  console.log(a + ' bigger than ' +b) : console.log(b + ' bigger than ' + a)
